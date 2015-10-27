13de93faaf3faf695b49d8dd5926d6e6bf4ab404
Chris Chapman <chris@pentandra.com>
2015-10-27 16:47:02 -0600

    Added svg-pan-zoom

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+window.addEventListener('DOMContentLoaded', function() {
 
+  var linkedDataGraph = svgPanZoom('#linked-data-graph', {
+    zoomEnabled: true,
+    controlIconsEnabled: true
+  });
+
+});
